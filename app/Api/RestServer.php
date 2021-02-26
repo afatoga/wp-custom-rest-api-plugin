ab62bd80316405ab3fa6a544789f4980df06bb05
afatoga <afatoga@gmail.com>
2021-02-26 16:36:06 +0100

    init

--- app/Api/RestServer.php
+++ app/Api/RestServer.php
@@ -2,7 +2,7 @@
 
 namespace Afatoga\Api;
 
-use Afatoga\Api\LinkController;
+//use Afatoga\Api\LinkController;
 
 class RestServer extends \WP_REST_Controller
 {
@@ -16,7 +16,7 @@ class RestServer extends \WP_REST_Controller
   }
 
   //The namespace and version for the REST SERVER
-  var $my_namespace = "af_restserver/v";
+  var $my_namespace = "aa_restserver/v";
   var $my_version   = "1";
 
   public function register_routes()
@@ -29,7 +29,7 @@ class RestServer extends \WP_REST_Controller
       [
         [
           "methods"         => "POST",
-          "callback"        => [$this, "af_register_new_user"],
+          "callback"        => [$this, "aa_register_new_user"],
           "permission_callback" => "__return_true",
         ]
       ]
@@ -41,8 +41,8 @@ class RestServer extends \WP_REST_Controller
       [
         [
           "methods"         => "POST",
-          "callback"        => [$this, "af_update_user_data"],
-          "permission_callback" => [$this, "af_is_user_logged_in"],
+          "callback"        => [$this, "aa_update_user_data"],
+          "permission_callback" => [$this, "aa_is_user_logged_in"],
         ]
       ]
     );
@@ -53,121 +53,27 @@ class RestServer extends \WP_REST_Controller
       [
         [
           "methods"         => "GET",
-          "callback"        => [$this, "af_get_user_data"],
-          "permission_callback"   => [$this, "af_is_user_logged_in"]
-        ],
-      ]
-    );
-
-    register_rest_route(
-      $namespace,
-      "/get_product",
-      [
-        [
-          "methods"         => "GET",
-          "callback"        => [$this, "af_get_product"],
-          "permission_callback"   => [$this, "af_is_user_logged_in"]
-        ],
-      ]
-    );
-    register_rest_route(
-      $namespace,
-      "/get_productlist",
-      [
-        [
-          "methods"         => "GET",
-          "callback"        => [$this, "af_get_productlist"],
-          "permission_callback"   => [$this, "af_is_user_logged_in"]
-        ],
-      ]
-    );
-    register_rest_route(
-      $namespace,
-      "/get_linklist",
-      [
-        [
-          "methods"         => "GET",
-          "callback"        => [$this, "af_get_linklist"],
-          "permission_callback"   => [$this, "af_is_user_logged_in"]
+          "callback"        => [$this, "aa_get_user_data"],
+          "permission_callback"   => [$this, "aa_is_user_logged_in"]
         ],
       ]
     );
   }
 
-  public function af_is_user_logged_in(\WP_REST_Request $request)
+  public function aa_is_user_logged_in(\WP_REST_Request $request)
   {
 
-    $payload = $request->get_params();
-    $route = $request->get_route();
-    $hash = filter_var($payload["h"], FILTER_SANITIZE_STRING);
-    if (strpos($hash,"7150ab71b8") === 0 && ($route === "/af_restserver/v1/get_product" || $route === "/af_restserver/v1/get_productlist")) return true;
+    //$payload = $request->get_params();
+    //$route = $request->get_route(); // $route === "/aa_restserver/v1/get_product"
 
     if (!$this->logged_in) {
-      //return true;
       return new \WP_Error("rest_forbidden", "Access forbidden", ["status" => 401]);
     }
     // !current_user_can("read")
     return true;
   }
 
-  public function af_get_linklist(\WP_REST_Request $request)
-  {
-    $linkController = new LinkController();
-    $payload = $request->get_params();
-    $currency = filter_var($payload["currency"], FILTER_SANITIZE_STRING);
-
-    $currencyList = ["CZK", "USD", "EUR", "RUB", "GBP"];
-
-    if (!$currency || !in_array($currency, $currencyList)) {
-      return new \WP_Error("rest_not_found", "Currency not found", ["status" => 404]);
-    }
-
-    $currency = strtolower($currency);
-    $linkList = $linkController->getLinkList($currency);
-
-    return new \WP_REST_Response(
-      ["data" => $linkList],
-      200
-    );
-  }
-
-  public function af_get_productlist(\WP_REST_Request $request)
-  {
-    $productController = new ProductController();
-    $payload = $request->get_params();
-    $hash = (isset($payload["h"])) ? filter_var($payload["h"], FILTER_SANITIZE_STRING) : null;
-    $limit = (isset($payload["limit"])) ? filter_var($payload["limit"], FILTER_VALIDATE_INT) : 10;
-    $offset = (isset($payload["offset"])) ? filter_var($payload["offset"], FILTER_VALIDATE_INT) : 0;
-
-    if (!$hash) return new \WP_Error("rest_bad_request", "Hash not valid", ["status" => 400]);
-
-    $response = $productController->getProductList($hash, $limit, $offset);
-    if (empty($response["productList"])) return new \WP_Error("rest_not_found", "Products not found", ["status" => 404]);
-
-    return new \WP_REST_Response(
-      ["data" => $response],
-      200
-    );
-  }
-
-  public function af_get_product(\WP_REST_Request $request)
-  {
-    $productController = new ProductController();
-    $payload = $request->get_params();
-    //$currency = filter_var($payload["currency"], FILTER_SANITIZE_STRING);
-    $hash = filter_var($payload["h"], FILTER_SANITIZE_STRING);
-
-    $productDetail = $productController->getProductDetail($this->logged_in, $hash);
-
-    if (empty($productDetail)) return new \WP_Error("rest_not_found", "Product not found", ["status" => 404]);
-
-    return new \WP_REST_Response(
-      $productDetail,
-      200
-    );
-  }
-
-  public function af_register_new_user(\WP_REST_Request $request)
+  public function aa_register_new_user(\WP_REST_Request $request)
   {
     if (!$this->user) return wp_send_json_error("400");
 
@@ -182,10 +88,11 @@ class RestServer extends \WP_REST_Controller
     );
   }
 
-  public function af_get_user_data()
+  public function aa_get_user_data()
   {
     if (!$this->user) return wp_send_json_error("400");
 
+    //accessing user meta in a convenient way
     $all_meta_for_user = array_map(function ($a) {
       return $a[0];
     }, get_user_meta($this->user->ID));
@@ -194,11 +101,11 @@ class RestServer extends \WP_REST_Controller
       "email" => $this->user->user_email,
       "first_name" => $this->user->first_name,
       "last_name" => $this->user->last_name,
-      "phone_number" => $all_meta_for_user["gemz_phone_number"],
-      "street" => $all_meta_for_user["gemz_street"],
-      "city" => $all_meta_for_user["gemz_city"],
-      "zip" => $all_meta_for_user["gemz_zip"],
-      "country" => $all_meta_for_user["gemz_country"],
+      "phone_number" => $all_meta_for_user["aa_phone_number"],
+      "street" => $all_meta_for_user["aa_street"],
+      "city" => $all_meta_for_user["aa_city"],
+      "zip" => $all_meta_for_user["aa_zip"],
+      "country" => $all_meta_for_user["aa_country"],
     ];
 
     return new \WP_REST_Response(
@@ -209,7 +116,7 @@ class RestServer extends \WP_REST_Controller
 
 
 
-  public function af_update_user_data(\WP_REST_Request $request)
+  public function aa_update_user_data(\WP_REST_Request $request)
   {
     if (!$this->user) return wp_send_json_error("400");
     $userId = $this->user->ID;
@@ -227,11 +134,11 @@ class RestServer extends \WP_REST_Controller
     //if (!$email) return wp_send_json_error("Invalid email", 400);
     update_user_meta($userId, 'first_name', $firstName);
     update_user_meta($userId, 'last_name', $lastName);
-    update_user_meta($userId, 'gemz_phone_number', $phoneNumber);
-    update_user_meta($userId, 'gemz_street', $street);
-    update_user_meta($userId, 'gemz_city', $city);
-    update_user_meta($userId, 'gemz_zip', $zip);
-    update_user_meta($userId, 'gemz_country', $country);
+    update_user_meta($userId, 'aa_phone_number', $phoneNumber);
+    update_user_meta($userId, 'aa_street', $street);
+    update_user_meta($userId, 'aa_city', $city);
+    update_user_meta($userId, 'aa_zip', $zip);
+    update_user_meta($userId, 'aa_country', $country);
 
     return new \WP_REST_Response(
       ["message" => "success"],
--- plugin.php
+++ plugin.php
@@ -6,10 +6,10 @@ use Afatoga\Api\RestServer;
 /**
  * Plugin.
  *
- * @package af-restserver
+ * @package af-restapi
  * @wordpress-plugin
  *
- * Plugin Name:     af_restserver
+ * Plugin Name:     aa_restapi
  * Description:     REST API for a custom theme
  * Author:          Afatoga
  * Author URL:      https://leoweb.cz
@@ -31,30 +31,30 @@ $loader->addNamespace( 'Afatoga', plugin_dir_path( __FILE__ ).'/app' );
  */
 
 add_action("rest_api_init", function () {
-  $af_restserver = new RestServer();
-  $af_restserver->register_routes();
+  $aa_restserver = new RestServer();
+  $aa_restserver->register_routes();
 });
 
 /**
- * Register the /wp-json/af_restserver/v1/product endpoint not to request authentication
+ * Register the /wp-json/aa_restserver/v1/product endpoint not to request authentication
  */
 
-add_filter( 'jwt_auth_whitelist', function ( $endpoints ) {
-  return [
-      '/wp-json/af_restserver/v1/get_product',
-      '/wp-json/af_restserver/v1/get_productlist'
-  ];
-} );
+// add_filter( 'jwt_auth_whitelist', function ( $endpoints ) {
+//   return [
+//       '/wp-json/aa_restserver/v1/get_product',
+//       '/wp-json/aa_restserver/v1/get_productlist'
+//   ];
+// } );
 
 /**
- * Register the /wp-json/af_restserver/v1/get_productlist endpoint so it will be cached.
+ * Register the /wp-json/aa_restserver/v1/get_productlist endpoint so it will be cached.
  */
 
-function af_add_productlist_endpoint( $allowed_endpoints ) {
-    if ( ! isset( $allowed_endpoints[ 'af_restserver/v1' ] ) || 
-         ! in_array( 'get_productlist', $allowed_endpoints[ 'af_restserver/v1' ] ) ) {
-        $allowed_endpoints[ 'af_restserver/v1' ][] = 'get_productlist';
+function aa_add_productlist_endpoint( $allowed_endpoints ) {
+    if ( ! isset( $allowed_endpoints[ 'aa_restserver/v1' ] ) || 
+         ! in_array( 'get_productlist', $allowed_endpoints[ 'aa_restserver/v1' ] ) ) {
+        $allowed_endpoints[ 'aa_restserver/v1' ][] = 'get_productlist';
     }
     return $allowed_endpoints;
 }
-//add_filter( 'wp_rest_cache/allowed_endpoints', 'af_add_productlist_endpoint', 10, 1);
+//add_filter( 'wp_rest_cache/allowed_endpoints', 'aa_add_productlist_endpoint', 10, 1);
